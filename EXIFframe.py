2a16ed47af901560d37cbdc9596f2410f4651301
7ryo <7hoshiryo@gmail.com>
2024-02-09 00:52:53 +0800

    v1.0

---
+++ EXIFframe.py
@@ -0,0 +1,196 @@
+from PIL import Image, ImageDraw, ImageFont, ImageOps
+import pathlib
+import pyexiv2
+from fractions import Fraction
+from nikon_lens import LensMapDict
+
+mytags = ["Make", "Model", "ExposureTime", "FNumber", "ISOSpeedRatings", "DateTimeOriginal", "LensModel", "FocalLength"]
+default_exif_message = "N/A"
+color_black = (0, 0, 0)
+color_gray = (100, 100, 100)
+selected_logo = 'Nikon_yellow'
+output_dir = '/output'
+output_suffix = '_with_frame'
+
+def convert_to_printable_date(string):
+    string = string[:10]
+    string = string.replace(':', '/')
+    return string
+
+def convert_to_hex(val):
+    hexstr = str(hex(int(val))).replace('0x', '').upper()
+    if len(hexstr) == 1:
+        hexstr = f"0{hexstr}"
+    return hexstr
+
+def get_nikon_lensname(all_data):
+    lensname = default_exif_message
+    LensID = ""
+    FStop = ""
+    MinFocalLength = ""
+    MaxFocalLength = ""
+    MaxApertureAtMinFocal = ""
+    MaxApertureAtMaxFocal = ""
+    MCUVersion = ""
+    LensType = ""
+    for key, val in all_data.items():
+        if "LensID" in key:
+            LensID = convert_to_hex(val)
+        if "FStop" in key:
+            subs = val.split(' ')
+            FStop = convert_to_hex(subs[0])
+        if "MinFocalLength" in key:
+            MinFocalLength = convert_to_hex(val)
+        if "MaxFocalLength" in key:
+            MaxFocalLength = convert_to_hex(val)
+        if "MaxApertureAtMaxFocal" in key:
+            MaxApertureAtMaxFocal = convert_to_hex(val)
+        if "MaxApertureAtMinFocal" in key:
+            MaxApertureAtMinFocal = convert_to_hex(val)
+        if "MCUVersion" in key:
+            MCUVersion = convert_to_hex(val)
+        if "LensType" in key:
+            LensType = convert_to_hex(val)
+
+        querystr = f"{LensID} {FStop} {MinFocalLength} {MaxFocalLength} {MaxApertureAtMinFocal} {MaxApertureAtMaxFocal} {MCUVersion} {LensType}"
+        if querystr in LensMapDict:
+            lensname = LensMapDict[querystr]    
+    
+    return lensname
+
+
+def get_exif_data(image_path):
+    exif_data = {}
+    img = pyexiv2.Image(image_path, encoding='big5')
+    all_data = img.read_exif()
+    #img.close
+
+    for key, val in all_data.items():
+        if "Exif.Image.Make" in key:
+            if val == "NIKON CORPORATION":
+                exif_data["LensModel"] = get_nikon_lensname(all_data)
+            else:
+                exif_data["Make"] = val
+        if "Exif.Image.Model" in key:
+            exif_data["Model"] = val
+        if "Exif.Photo.ExposureTime" in key:
+            exif_data["ExposureTime"] = str(Fraction(val))
+        if "Exif.Photo.FNumber" in key:
+            exif_data["FNumber"] = float(Fraction(val))
+        if "Exif.Photo.ISOSpeedRatings" in key:
+            exif_data["ISO"] = val
+        if "Exif.Photo.DateTimeOriginal" in key:
+            exif_data["DateTimeOriginal"] = convert_to_printable_date(val)
+        if "Exif.Photo.FocalLength" in key:
+            exif_data["FocalLength"] = str(Fraction(val))
+        if "Exif.Photo.LensModel" in key:
+            exif_data["LensModel"] = val
+        
+    
+    #for key, val in exif_data.items():
+    #    print(f"{key} = {val}")
+
+    return exif_data
+
+def draw_watermark_frame(image_path='path', bool_output=False, input_logo=selected_logo):
+    exif_data = get_exif_data(image_path)
+    mm, iso, f, exptime = (
+        exif_data.get("FocalLength", default_exif_message),
+        exif_data.get("ISO", default_exif_message),
+        exif_data.get("FNumber", default_exif_message),
+        exif_data.get("ExposureTime", default_exif_message)
+    )
+    text_shotinfo = (
+        default_exif_message
+        if default_exif_message in (mm, iso, f, exptime)
+        else "{}mm  ISO{}  f{}  {}s".format(mm, iso, f, exptime)
+    )
+    text_model = "{}".format(exif_data.get("Model", default_exif_message))
+    text_lens = "{}".format(exif_data.get("LensModel", default_exif_message))
+    text_datetime = "{}".format(exif_data.get("DateTimeOriginal", default_exif_message))
+
+    image_original = Image.open(image_path).convert("RGBA")
+    image_original = ImageOps.exif_transpose(image_original)
+
+    #set sizes
+    #image_size[0] is width; image_size[1] is height
+    image_size = image_original.size
+    blank_height = int(min(image_size[0], image_size[1])/10)
+    padding = int(blank_height/5)
+    padding_left = padding
+    font_size = int(blank_height/4)
+    font_reualr = ImageFont.truetype("./Saira_Semi_Condensed/SairaSemiCondensed-Regular.ttf", size=font_size)
+    font_bold = ImageFont.truetype("C:/Users/suwen/Desktop/bot/Saira_Semi_Condensed/SairaSemiCondensed-Bold.ttf", size=font_size)
+    logo_max_height = int(blank_height/1.5)
+
+    #paste original image to blank
+    img = Image.new("RGB", (image_size[0], image_size[1] + blank_height), (255, 255, 255))
+    img.paste(image_original, (0, 0), image_original)
+
+    draw = ImageDraw.Draw(img)
+    padding_top = padding + image_size[1] #position
+
+    #draw texts
+    draw.text((padding_left, padding_top), text_shotinfo, fill=color_black, font=font_bold)
+    draw.text((padding_left, padding_top + padding + padding//3), text_datetime, fill=color_gray, font=font_reualr)
+    max_len = max(font_bold.getlength(text_model), font_reualr.getlength(text_lens))
+    position_make = (image_size[0] - padding - max_len, padding_top)
+    position_lens = (image_size[0] - padding - max_len, padding_top + padding + padding//3)
+    draw.text(position_make, text_model, fill=color_black, font=font_bold, align="left")
+    draw.line(
+        (
+            (position_make[0] - padding//2, position_make[1]),
+            (position_lens[0] - padding//2, padding_top + padding*3),
+        ),
+        fill=color_gray,
+        width=int(font_size/8)
+    )
+    draw.text(position_lens, text_lens, fill=color_gray, font=font_reualr)
+
+    #draw logo
+    logo = Image.open(f"logo/{input_logo}.png")
+    width, height = logo.size
+    new_width = int((logo_max_height/height) * width)
+    logo = logo.resize((new_width, logo_max_height), Image.LANCZOS).convert("RGBA")
+
+    img.paste(
+        logo,
+        (
+            int(image_size[0] - max_len - padding - logo.size[0] - padding),
+            image_size[1] + int((blank_height - logo_max_height)/2)
+        ),
+        logo
+    )
+
+    if bool_output == True:
+        original_path = pathlib.Path(image_path)
+        output_path = f"{output_dir}/{original_path.stem}{output_suffix}{original_path.suffix}"
+        img.save(output_path)
+    else:
+        #resize to fit the canvas for preview
+        img_width, img_height = img.size
+        new_img_width=0
+        new_img_height=0
+        if (img_width < img_height): #portrait
+            new_img_height = 440
+            new_img_width = int((new_img_height/img_height) * img_width)
+        else: #landscape
+            new_img_width = 600
+            new_img_height = int((new_img_width/img_width) * img_height)
+        img = img.resize((new_img_width, new_img_height), Image.LANCZOS)
+    return img
+    
+
+
+
+def generate(img_path_list, logo, dir, suffix):
+    global selected_logo
+    global output_dir
+    global output_suffix
+    selected_logo = (logo)
+    output_dir = dir
+    output_suffix = suffix
+    for current_path in img_path_list:
+        draw_watermark_frame(current_path, bool_output=True)
+    return 0
+
---
+++ mainApp.py
@@ -0,0 +1,200 @@
+import tkinter
+from tkinter import filedialog
+import customtkinter
+from PIL import ImageTk, Image
+import os
+import EXIFframe
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+
+bool_firstimport = True
+tk_img = None
+img_preview_id = None
+outputdir = '/output'
+first_file_path = None
+class App(customtkinter.CTk):
+
+
+    def runWatermark():
+        #for e in selected_img_path:
+            #utility.draw_watermark_frame(e)
+        #selected_img_path = []
+        return 0
+
+    def __init__(self):
+        super().__init__()
+
+        self.title("EXIF WATERMARK MAKER")
+        self.geometry(f"{800}x{600}")
+        self.maxsize(800, 600)
+
+        def addtolistbox(file_path):
+            self.listbox_importedfile.insert("end", file_path)
+            global tk_img
+            global bool_firstimport
+            global img_preview_id
+            global first_file_path
+            if bool_firstimport == True:
+                first_file_path = file_path
+                previewimg = get_preview_img()
+                tk_img = ImageTk.PhotoImage(image=previewimg)
+                img_preview_id = self.canvas_preview.create_image(338, 227, anchor='center', image=tk_img)
+                bool_firstimport = False
+        def removefromlistbox():
+            global bool_firstimport
+            global img_preview_id
+            global tk_img
+            global first_file_path
+            selected_index = self.listbox_importedfile.curselection()
+            for i in reversed(selected_index): #tuple
+                if (i == 0) and (self.listbox_importedfile.size() > 1):
+                    first_file_path = self.listbox_importedfile.get(1)
+                    get_preview_img()
+                self.listbox_importedfile.delete(i)
+            if self.listbox_importedfile.size() == 0:
+                #listbox is empty
+                bool_firstimport = True
+                self.canvas_preview.delete(img_preview_id)
+                img_preview_id = None
+
+        def get_preview_img(image_path=first_file_path):
+            global tk_img
+            global first_file_path
+            
+            logo = self.combobox_logo.get()
+            previewimg = EXIFframe.draw_watermark_frame(image_path=first_file_path, input_logo=logo)
+            if bool_firstimport == False:
+                tk_img = ImageTk.PhotoImage(previewimg)
+                self.canvas_preview.itemconfig(img_preview_id, image=tk_img)
+            return previewimg
+        def removefromlistbox_all():
+            global bool_firstimport
+            global img_preview_id
+            self.listbox_importedfile.delete(0, tkinter.END)
+            if self.listbox_importedfile.size() == 0:
+                #listbox is empty
+                bool_firstimport = True
+                self.canvas_preview.delete(img_preview_id)
+                img_preview_id = None
+
+        def browse_files():
+            files = filedialog.askopenfilenames(initialdir=getcurrentdir(),
+                                                title = "Select a File or Files",
+                                                filetypes=(("JPG image", "*.jpg"),
+                                                            ("PNG image", "*.png")
+                                                            ))
+            #add selected file into listbox
+            for f in files:
+                addtolistbox(f)
+
+        def load_combobox_options(dir):
+            list_logos = []
+            list_filenames = os.listdir(dir)
+            for n in list_filenames:
+                list_logos.append(n.replace('.png', ''))
+            return list_logos
+
+        def getcurrentdir():
+            cwd = os.getcwd()
+            cwd = cwd.replace("\\", "/")
+            return cwd
+
+        def browse_directory():
+            global outputdir
+            selecteddir = filedialog.askdirectory(initialdir=f"{getcurrentdir()}",
+                                          title="Select a Folder")
+            if selecteddir:
+                text_outputpath.set(selecteddir)
+                outputdir = selecteddir
+
+        def getvalues():
+            self.progressbar_run.grid(row=8, column=1, padx=20, pady=10)
+            self.progressbar_run.start()
+            self.button_run['state'] = tkinter.DISABLED
+            paths = self.listbox_importedfile.get(0, tkinter.END)
+            logo = self.combobox_logo.get()
+            dir = self.entry_outputpath.get()
+            if dir == '':
+                dir = "/output"
+            text_entry = self.entry_outputsuffix.get()
+            suffix = "_with_frame"
+            if text_entry != '':
+                suffix = text_entry
+            i = EXIFframe.generate(paths, logo, dir, suffix)
+            if i == 0:
+                removefromlistbox_all()
+                self.progressbar_run.grid_forget()
+                self.button_run['state'] = tkinter.NORMAL
+
+            
+
+
+        # left frame: select files
+        self.left_frame = customtkinter.CTkFrame(self, width=400, corner_radius=0)
+        #self.left_frame.grid(row=0, column=0, columnspan=2, sticky="nsew")
+        self.left_frame.pack(fill='y', side='left')
+        self.left_frame.grid_rowconfigure(1, weight=1)
+
+        self.button_browsefile = customtkinter.CTkButton(self.left_frame, text="Browse Image", command=browse_files)
+        self.button_browsefile.grid(row=0, column=0, padx=20, pady=10, sticky="nesw")
+        self.listbox_importedfile = tkinter.Listbox(self.left_frame, width=60, justify="left",
+                                                    selectmode=tkinter.MULTIPLE, selectbackground="gray")
+        self.listbox_importedfile.grid(row=1, column=0, columnspan=2, padx=20, pady=10, sticky="nesw")
+        # function: get listbox option
+        self.button_removefile = customtkinter.CTkButton(self.left_frame, text="Remove", command=removefromlistbox)
+        self.button_removefile.grid(row=4, column=0, padx=20, pady=10, sticky="nesw")
+        self.button_removefileall = customtkinter.CTkButton(self.left_frame, text="Remove All", command=removefromlistbox_all)
+        self.button_removefileall.grid(row=5, column=0, padx=20, pady=10, sticky="nesw")
+        
+
+        # right frame: main
+        self.right_frame = customtkinter.CTkFrame(self, corner_radius=0)
+        #self.right_frame.grid(row=0, column=2, columnspan=3, sticky="nsew")
+        self.right_frame.pack(fill='y', side='left')
+        self.right_frame.grid_rowconfigure(1, weight=1)
+
+        self.label_preview = customtkinter.CTkLabel(self.right_frame, text="Preview")
+        self.label_preview.grid(row=0, column=0, padx=20, pady=(10, 0), sticky="nesw")
+        self.canvas_preview = tkinter.Canvas(self.right_frame, relief="solid")
+        self.canvas_preview.grid(row=1, columnspan=3, padx=40, pady=(10,20), sticky="nesw")
+        # (canvas size is 382 x 268)
+        self.label_logo = customtkinter.CTkLabel(self.right_frame, text="Logo")
+        self.label_logo.grid(row=2, column=0, padx=20, pady=10, sticky="nsw")
+        logo_option_list = load_combobox_options('./logo')
+        default_var = customtkinter.StringVar()
+        default_var.set(logo_option_list[0])
+        self.combobox_logo = customtkinter.CTkComboBox(self.right_frame, 
+                                                       values=logo_option_list, state="readonly", variable=default_var,
+                                                       command=get_preview_img)
+        self.combobox_logo.grid(row=2, column=1, padx=20, pady=10, sticky="nesw")
+
+        self.label_outputpath = customtkinter.CTkLabel(self.right_frame, text="Output Path (Please Contain Only English)")
+        self.label_outputpath.grid(row=3, column=0, columnspan=2, padx=20, pady=10, sticky="nsw")
+        self.button_browsepath = customtkinter.CTkButton(self.right_frame, text="Browse", command=browse_directory)
+        self.button_browsepath.grid(row=3, column=2, padx=20, pady=10)
+        text_outputpath = customtkinter.StringVar()
+        text_outputpath.set(f"{getcurrentdir()}/output")
+        self.entry_outputpath = customtkinter.CTkEntry(self.right_frame, textvariable=text_outputpath, state=tkinter.DISABLED)
+        self.entry_outputpath.grid(row=4, column=0, columnspan=3, padx=20, pady=10, sticky="nesw")
+
+
+        self.label_outputname = customtkinter.CTkLabel(self.right_frame, text="Output Filename")
+        self.label_outputname.grid(row=5, column=0, padx=20, pady=10, sticky="nsw")
+        self.label_outputstem = customtkinter.CTkLabel(self.right_frame, text="original filename")
+        self.label_outputstem.grid(row=5, column=1, padx=10, pady=10)
+        text_suffix = customtkinter.StringVar()
+        text_suffix.set("_with_frame")
+        self.entry_outputsuffix = customtkinter.CTkEntry(self.right_frame, textvariable=text_suffix)
+        self.entry_outputsuffix.grid(row=5, column=2, padx=10, pady=10)
+
+        self.progressbar_run = customtkinter.CTkProgressBar(self.right_frame, width=100, mode="indeterminate")
+        
+        self.button_run = customtkinter.CTkButton(self.right_frame, text="Run", command=getvalues)
+        self.button_run.grid(row=8, column=2, padx=20, pady=10)
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
